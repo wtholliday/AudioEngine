4a8f6bc5d65565349fa29c2af7c9189acee0fa06
Taylor Holliday <wtholliday@gmail.com>
2017-07-25 12:33:42 -0700

    Process audio.

--- AudioEngineExample/AudioEngineExample/AppDelegate.m
+++ AudioEngineExample/AudioEngineExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "AmAudioEngine.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [AmAudioEngine registerAudioUnit];
+    
+    [[AmAudioEngine sharedEngine] initAudio];
+    
     return YES;
 }
 
